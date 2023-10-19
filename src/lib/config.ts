e8d5729004543682b40c31f5a4338e809c23bdcb
Shindra David <shindradavid.dev@gmail.com>
2023-10-19 15:49:37 +0300

    Initial commit

---
+++ src/lib/config.ts
@@ -0,0 +1,4 @@
+// Example `https://api.themoviedb.org/3/movie/550?api_key=abf033c78e8861030b2a41e962dada52`
+
+export const baseUrl = "https://api.themoviedb.org/3"
+export const baseImageUrl = "https://image.tmdb.org/t/p"
---
+++ src/routes/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Shindra's Movie Database</h1>
